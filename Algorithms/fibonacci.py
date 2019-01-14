9aeaa1000d3088b2189285095873c51c95800295
imahnama <muannama25@gmail.com>
2019-01-14 20:18:10 +0300

    Solution to fibonacci sequence

--- Algorithms/fibonacci.py
+++ Algorithms/fibonacci.py
@@ -1,5 +1,10 @@
-# not yet done with this
-a, b = 0,1
-c = b + (b-1)
-while c < 10:
-    print(c)
+def fib(n):
+  if n == 0:
+      return 0
+  elif n == 1:
+    return 1
+  else:
+    return fib(n-1) + fib(n-2)
+
+for i in range(1, 10):  # print first 10 numbers
+    print(fib(i))
