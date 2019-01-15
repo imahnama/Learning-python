fd0351a896a46e1185e44e6d59386852a9d46361
Rahma Abdi <muannama25@gmail.com>
2019-01-15 23:41:46 +0300

    renamed function to a more descriptive name

--- Algorithms/fibonacci.py
+++ Algorithms/fibonacci.py
@@ -1,10 +1,10 @@
-def fib(n):
+def fibonacci(n):
   if n == 0:
       return 0
   elif n == 1:
     return 1
   else:
-    return fib(n-1) + fib(n-2)
+    return fibonacci(n-1) + fibonacci(n-2)
 
 for i in range(1, 10):  # print first 10 numbers
-    print(fib(i))
+    print(fibonacci(i))
